4fa0dc5ae8d4d02e1c4cdd91b57feacf018c56d9
Livara <livara@gmail.com>
2023-08-14 22:13:42 +0200

    Add cards to the catsitting page and content

--- cat-and-everything/resources/views/catsitting/cat.blade.php
+++ cat-and-everything/resources/views/catsitting/cat.blade.php
@@ -8,29 +8,33 @@
 <div class="container mt-5">
     <div class="row">
       <div class="col-md-4">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body text-center">
             <img src="your_image_url1.jpg" class="img-fluid" alt="Image 1">
-            <h5 class="card-title mt-3">Card 1</h5>
-            <p class="card-text">Some text related to Card 1.</p>
+            <h5 class="card-title mt-3">1st visit</h5>
+            <p class="card-text">I know you want a cat sitter that can trust, that's why I am giving a 1st visit free of charge to try me and see if you and your feline will like my service.
+                You can avail the free 1st visit if you book 2 days visit in one time.
+            </p>
           </div>
         </div>
       </div>
       <div class="col-md-4">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body text-center">
             <img src="your_image_url2.jpg" class="img-fluid" alt="Image 2">
-            <h5 class="card-title mt-3">Card 2</h5>
-            <p class="card-text">Some text related to Card 2.</p>
+            <h5 class="card-title mt-3">1 visit a day</h5>
+            <p class="card-text">This visit is only 1x a day and I'll stay for 30mins with your feline and the owner can choose between morning and evening. Give water and food, clean the litterbox and play with your feline. </p>
           </div>
         </div>
       </div>
       <div class="col-md-4">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body text-center">
             <img src="your_image_url3.jpg" class="img-fluid" alt="Image 3">
-            <h5 class="card-title mt-3">Card 3</h5>
-            <p class="card-text">Some text related to Card 3.</p>
+            <h5 class="card-title mt-3">2 visits a day</h5>
+            <p class="card-text">This visit will be in the morning and in the evening. I'll stay for 25mins per visit. Give water and food, clean the litterbox and play with your feline.
+                Cats who needs special care is important for me to know. Please include in this in your booking details.
+            </p>
           </div>
         </div>
       </div>
