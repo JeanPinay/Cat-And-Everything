27a3f33d4b426fe7f906a9e6a04a6cb2b2369f43
Livara <livara@gmail.com>
2023-08-14 22:32:06 +0200

    Add cards to the catsitting page and content

--- cat-and-everything/resources/views/layouts/footer.blade.php
+++ cat-and-everything/resources/views/layouts/footer.blade.php
@@ -65,9 +65,9 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h5 class="text-uppercase fw-bold mb-4">
             Services
-          </h6>
+          </h5>
           <p>
             <a href="#!" class="text-reset">Cat Sitting</a>
           </p>
@@ -86,9 +86,9 @@
         <!-- Grid column -->
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h5 class="text-uppercase fw-bold mb-4">
             Useful links
-          </h6>
+          </h5>
           <p>
             <a href="{{ route('contact-us') }}" class="text-reset">Contact Us</a>
           </p>
@@ -104,7 +104,7 @@
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">Address</h6>
+          <h5 class="text-uppercase fw-bold mb-4">Address</h5>
           <p><i class="fas fa-home me-3 text-secondary"></i> Deurne 2100, Antwerpen, BE</p>
           <p>
             <i class="fas fa-envelope me-3 text-secondary"></i>
